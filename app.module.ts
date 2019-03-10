c5c6e91ff14d840368250977c0351494abf685b7
pushpa <47204528+pushpa1998@users.noreply.github.com>
2019-03-10 17:40:43 +0530

    sample
    {{title}}

--- app.module.ts
+++ app.module.ts
@@ -1,41 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {RouterModule, Routes} from '@angular/router';
-import { HttpClientModule } from '@angular/common/http';
-
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component';
-import { GalleryComponent } from './gallery/gallery.component';
-import { ContactComponent } from './contact/contact.component';
-import { LoginComponent } from './login/login.component';
-import { ServiceComponent } from './service/service.component';
-const appRoutessss: Routes = [
-  {path: 'home', component: HomeComponent},
-  {path: 'about', component: AboutComponent},
-  {path: 'gallery', component: GalleryComponent},
-  {path: 'contact', component: ContactComponent},  
-  {path: 'login', component: LoginComponent},
-  {path: 'service', component: ServiceComponent}
-];
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomeComponent,
-    AboutComponent,
-    GalleryComponent,
-    ContactComponent,
-    LoginComponent,
-    ServiceComponent
-  
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    RouterModule.forRoot(appRoutessss),
-    HttpClientModule
+    BrowserModule
   ],
-  
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
